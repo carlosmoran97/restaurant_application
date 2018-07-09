7a8daff6cf98c83c1b30fe8592df5cd79dad0c60
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-09 10:52:32 -0600

    Agregando la aplicaci

--- restaurant_project/restaurant_project/settings.py
+++ restaurant_project/restaurant_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'restaurant_application'
+    'restaurant_application',
+    'inventario'
 ]
 
 MIDDLEWARE = [
