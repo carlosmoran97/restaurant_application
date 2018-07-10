0e9a8d88dbbf7ac53b516abd14ee9be77a93e3bb
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-09 22:31:41 -0600

    inventario

--- restaurant_project/inventario/urls.py
+++ restaurant_project/inventario/urls.py
@@ -5,5 +5,9 @@ app_name = 'inventario'
 
 urlpatterns = [
     url(r'^productos/$', views.productos, name='productos'),
-    
+    url(r'^productos_list/$', views.Get_producto_List, name='productos_list'),
+    url(r'^productos_filter/$', views.Get_producto_ListFilter.as_view(), name='productos_filter'),
+    url(r'^productos_create/$', views.Get_producto_Create.as_view(), name='productos_create'),
+    url(r'^productos_update/$', views.Get_producto_Update.as_view(), name='productos_update'),
+    url(r'^productos_delete/$', views.Get_producto_Delete.as_view(), name='productos_delete'),
 ]
