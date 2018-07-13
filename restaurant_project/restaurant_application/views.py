2e92bc4193e4f16e14c35203f6e16f02cf537b37
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-13 17:16:34 -0600

    cambiando todos los post a get

--- restaurant_project/restaurant_application/views.py
+++ restaurant_project/restaurant_application/views.py
@@ -16,30 +16,30 @@ def index_categoria_platillos(request):
     return render(request, 'restaurant_application/categoria_platillos/index.html', context)
 
 class CategoriaPlatilloCreate(APIView):
-    def post(self, request):
-        categoriaplatillo = CategoriaPlatillo.objects.create(categoria=request.POST['categoria'])
+    def get(self, request):
+        categoriaplatillo = CategoriaPlatillo.objects.create(categoria=request.GET['categoria'])
         categoriaplatillo.save()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class CategoriaPlatilloUpdate(APIView):
-    def post(self, request):
-        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.POST['idCategoriaPlatillo']).update(categoria=request.POST['categoria'])
+    def get(self, request):
+        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.GET['idCategoriaPlatillo']).update(categoria=request.GET['categoria'])
         return JsonResponse({'respuesta':'correctamente!'})
 
 class CategoriaPlatilloDelete(APIView):
-    def post(self, request):
-        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.POST['idCategoriaPlatillo']).delete()
+    def get(self, request):
+        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.GET['idCategoriaPlatillo']).delete()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class CategoriaPlatilloList(APIView):
-    def post(self, request):
+    def get(self, request):
         categoriaplatillos = CategoriaPlatillo.objects.all().order_by("-idCategoriaPlatillo")
         serialized = CategoriaPlatilloSerializer(categoriaplatillos, many=True)
         return Response(serialized.data)
 
 class CategoriaPlatilloListFilter(APIView):
-    def post(self, request):
-        categoriaplatillos = CategoriaPlatillo.objects.filter(categoria__startswith=request.POST['categoria']).order_by("-idCategoriaPlatillo")
+    def get(self, request):
+        categoriaplatillos = CategoriaPlatillo.objects.filter(categoria__startswith=request.GET['categoria']).order_by("-idCategoriaPlatillo")
         serialized = CategoriaPlatilloSerializer(categoriaplatillos, many=True)
         return Response(serialized.data)
 
@@ -53,25 +53,25 @@ def index_platillos(request):
     return render(request, 'restaurant_application/platillos/index.html', context)
 
 class PlatilloCreate(APIView):
-    def post(self, request):
-        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.POST['categoria_platillo'])
-        platillo = Platillo.objects.create(nombre=request.POST['nombre'], precioUnitario=request.POST['precioUnitario'], categoria_platillo=categoriaplatillo[0])
+    def get(self, request):
+        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.GET['categoria_platillo'])
+        platillo = Platillo.objects.create(nombre=request.GET['nombre'], precioUnitario=request.GET['precioUnitario'], categoria_platillo=categoriaplatillo[0])
         platillo.save()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class PlatilloUpdate(APIView):
-    def post(self, request):
-        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.POST['categoria_platillo'])
-        platillo = Platillo.objects.filter(codigoPlatillo=request.POST['codigoPlatillo']).update(nombre=request.POST['nombre'], precioUnitario=request.POST['precioUnitario'], categoria_platillo=categoriaplatillo[0])
+    def get(self, request):
+        categoriaplatillo = CategoriaPlatillo.objects.filter(idCategoriaPlatillo=request.GET['categoria_platillo'])
+        platillo = Platillo.objects.filter(codigoPlatillo=request.GET['codigoPlatillo']).update(nombre=request.GET['nombre'], precioUnitario=request.GET['precioUnitario'], categoria_platillo=categoriaplatillo[0])
         return JsonResponse({'respuesta':'correctamente!'})
 
 class PlatilloDelete(APIView):
-    def post(self, request):
-        platillo = Platillo.objects.filter(codigoPlatillo=request.POST['codigoPlatillo']).delete()
+    def get(self, request):
+        platillo = Platillo.objects.filter(codigoPlatillo=request.GET['codigoPlatillo']).delete()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class PlatilloList(APIView):
-    def post(self, request):
+    def get(self, request):
         platillos = Platillo.objects.all().order_by("-codigoPlatillo")
         serialized = PlatilloSerializer(platillos, many=True)
         return Response(serialized.data)
@@ -86,23 +86,23 @@ def index_mesas(request):
     return render(request, 'restaurant_application/mesas/index.html', context)
 
 class MesaCreate(APIView):
-    def post(self, request):
-        mesa = Mesa.objects.create(numero_mesa=request.POST['numero_mesa'], asientos=request.POST['asientos'])
+    def get(self, request):
+        mesa = Mesa.objects.create(numero_mesa=request.GET['numero_mesa'], asientos=request.GET['asientos'])
         mesa.save()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class MesaUpdate(APIView):
-    def post(self, request):
-        mesa = Mesa.objects.filter(codigo_mesa=request.POST['codigo_mesa']).update(numero_mesa=request.POST['numero_mesa'], asientos=request.POST['asientos'])
+    def get(self, request):
+        mesa = Mesa.objects.filter(codigo_mesa=request.GET['codigo_mesa']).update(numero_mesa=request.GET['numero_mesa'], asientos=request.GET['asientos'])
         return JsonResponse({'respuesta':'correctamente!'})
 
 class MesaDelete(APIView):
-    def post(self, request):
-        mesa = Mesa.objects.filter(codigo_mesa=request.POST['codigo_mesa']).delete()
+    def get(self, request):
+        mesa = Mesa.objects.filter(codigo_mesa=request.GET['codigo_mesa']).delete()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class MesaList(APIView):
-    def post(self, request):
+    def get(self, request):
         mesas = Mesa.objects.all().order_by("-codigo_mesa")
         serialized = MesaSerializer(mesas, many=True)
         return Response(serialized.data)
@@ -120,34 +120,34 @@ def index_empleados(request):
 
 
 class EmpleadoCreate(APIView):
-    def post(self, request):
-        empleado = Empleado.objects.create(nombre=request.POST['nombre'], apellido=request.POST['apellido'], fecha_nacimiento=request.POST['fecha_nacimiento'], dui=request.POST['dui'], nit=request.POST['nit'], afp=request.POST['afp'])
-        puesto = Puesto.objects.filter(idPuesto=request.POST['idPuesto'])
-        asignacion = Asignacion.objects.create(empleado=empleado, puesto=puesto[0], salario=request.POST['salario'], fecha_contratacion=request.POST['fecha_contratacion'])
+    def get(self, request):
+        empleado = Empleado.objects.create(nombre=request.GET['nombre'], apellido=request.GET['apellido'], fecha_nacimiento=request.GET['fecha_nacimiento'], dui=request.GET['dui'], nit=request.GET['nit'], afp=request.GET['afp'])
+        puesto = Puesto.objects.filter(idPuesto=request.GET['idPuesto'])
+        asignacion = Asignacion.objects.create(empleado=empleado, puesto=puesto[0], salario=request.GET['salario'], fecha_contratacion=request.GET['fecha_contratacion'])
         return JsonResponse({'respuesta':'correctamente!'})
 
 
 class EmpleadoUpdate(APIView):
-    def post(self, request):
-        empleado = Empleado.objects.filter(idEmpleado=request.POST['idEmpleado']).update(nombre=request.POST['nombre'], apellido=request.POST['apellido'], fecha_nacimiento=request.POST['fecha_nacimiento'], dui=request.POST['dui'], nit=request.POST['nit'], afp=request.POST['afp'])
-        puesto = Puesto.objects.filter(idPuesto=request.POST['idPuesto'])
-        asignacion = Asignacion.objects.filter(id=request.POST['id']).update(puesto=puesto[0], salario=request.POST['salario'], fecha_contratacion=request.POST['fecha_contratacion'])
+    def get(self, request):
+        empleado = Empleado.objects.filter(idEmpleado=request.GET['idEmpleado']).update(nombre=request.GET['nombre'], apellido=request.GET['apellido'], fecha_nacimiento=request.GET['fecha_nacimiento'], dui=request.GET['dui'], nit=request.GET['nit'], afp=request.GET['afp'])
+        puesto = Puesto.objects.filter(idPuesto=request.GET['idPuesto'])
+        asignacion = Asignacion.objects.filter(id=request.GET['id']).update(puesto=puesto[0], salario=request.GET['salario'], fecha_contratacion=request.GET['fecha_contratacion'])
         return JsonResponse({'respuesta':'correctamente!'})
 
 class EmpleadoDelete(APIView):
-    def post(self, request):
-        empleado = Empleado.objects.filter(idEmpleado=request.POST['idEmpleado']).delete()
-        asignacion = Asignacion.objects.filter(id=request.POST['id']).delete()
+    def get(self, request):
+        empleado = Empleado.objects.filter(idEmpleado=request.GET['idEmpleado']).delete()
+        asignacion = Asignacion.objects.filter(id=request.GET['id']).delete()
         return JsonResponse({'respuesta':'correctamente!'})
 
 class EmpleadosList(APIView):
-    def post(self, request):
+    def get(self, request):
         asignaciones = Asignacion.objects.all().order_by("-id")
         serialized = AsignacionSerializer(asignaciones, many=True)
         return Response(serialized.data)
 
 class EmpleadoDetalle(APIView):
-    def post(self, request):
-        asignacion = Asignacion.objects.filter(id=request.POST['id'])
+    def get(self, request):
+        asignacion = Asignacion.objects.filter(id=request.GET['id'])
         serialized = AsignacionSerializer(asignacion, many=True)
         return Response(serialized.data)
