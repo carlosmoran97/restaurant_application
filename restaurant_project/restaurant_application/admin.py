c2715ae8ec20b2680b6e28abbea1de0cd63a55f0
Carlos Moran <carlosmoran.97cr@gmail.com>
2018-07-13 17:04:53 -0600

    anterior a implementar cstf tokens en las peticiones ajax

--- restaurant_project/restaurant_application/admin.py
+++ restaurant_project/restaurant_application/admin.py
@@ -1,7 +1,18 @@
 from django.contrib import admin
 from inventario.models import Producto, Existencia, ReporteDeExistencia
-# Register your models here.
+from restaurant_application.models import Asignacion, CategoriaPlatillo, Empleado, Mesa, Platillo, Puesto
+
+# modelos de la aplicación de inventario
 
 admin.site.register(Producto)
 admin.site.register(Existencia)
 admin.site.register(ReporteDeExistencia)
+
+# modelos de la aplicación de restaurante
+
+admin.site.register(Asignacion)
+admin.site.register(CategoriaPlatillo)
+admin.site.register(Empleado)
+admin.site.register(Mesa)
+admin.site.register(Platillo)
+admin.site.register(Puesto)
--- restaurant_project/restaurant_application/models.py
+++ restaurant_project/restaurant_application/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class CategoriaPlatillo(models.Model):
     idCategoriaPlatillo = models.AutoField(primary_key=True)
     categoria = models.CharField(max_length=200)
-
+    managed = True
     def __str__(self):
         return '{}'.format(self.categoria)
 
